2c56323b0eadcdf79a7c4c5cfb4eed7255c7d718
Alvine <alvnmugo@gmail.com>
2024-11-19 20:13:08 +0300

    update

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -7,9 +7,16 @@ function Type() {
       options={{
         strings: [
           "Software Developer",
+          "Full Stack Developer",
+          "React Developer",
+          "Node.js Developer",
+          "Python Developer",
+          "Java Developer",
+          "UI/UX Designer",
           "Freelancer",
           "MERN Stack Developer",
           "Open Source Contributor",
+          "cybersecurity enthusiastic Contributor"
         ],
         autoStart: true,
         loop: true,
